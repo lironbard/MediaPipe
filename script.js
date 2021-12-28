b7b5aee45361435dd355c01d3a1df2bc553e1cdf
lironbard <lironbard@gmail.com>
2021-12-29 00:00:00 +0200

    Initial

---
+++ script.js
@@ -0,0 +1,80 @@
+const videoElement = document.getElementsByClassName("input_video")[0];
+const canvasElement = document.getElementsByClassName("output_canvas")[0];
+const canvasCtx = canvasElement.getContext("2d");
+const landmarkContainer = document.getElementsByClassName("landmark-grid-container")[0];
+// const grid = new LandmarkGrid(landmarkContainer);
+
+function onResults(results) {
+  const printResult = () => {
+    let dotA = results.poseLandmarks[23]; // Left_hip
+    let dotB = results.poseLandmarks[25]; // Left_knee
+    let dotC = results.poseLandmarks[27]; // Left_ankle
+    // console.log(dotA);
+    // console.log(dotB);
+    // console.log(dotC);
+    let dotBx = results.poseLandmarks[25].x;
+    let dotBy = results.poseLandmarks[25].y;
+
+    let dotBz = results.poseLandmarks[12].z;
+    let dotBBz = results.poseLandmarks[11].z;
+    console.log(`The Z of your right Knee is ${dotBBz}`);
+    console.log(dotBx);
+    console.log(`The X of your left Knee is ${dotBx}`);
+    console.log(dotBy);
+    console.log(`The Y of your left Knee is ${dotBy}`);
+    console.log(dotBz);
+    console.log(`The Z of your left Knee is ${dotBz}`);
+  };
+
+  printResult();
+
+  // console.log(results.poseLandmarks);
+  if (!results.poseLandmarks) {
+    grid.updateLandmarks([]);
+    return;
+  }
+
+  canvasCtx.save();
+  canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+  canvasCtx.drawImage(results.segmentationMask, 0, 0, canvasElement.width, canvasElement.height);
+
+  // Only overwrite existing pixels.
+  canvasCtx.globalCompositeOperation = "source-in";
+  canvasCtx.fillStyle = "#00FF00";
+  canvasCtx.fillRect(0, 0, canvasElement.width, canvasElement.height);
+
+  // Only overwrite missing pixels.
+  canvasCtx.globalCompositeOperation = "destination-atop";
+  canvasCtx.drawImage(results.image, 0, 0, canvasElement.width, canvasElement.height);
+
+  canvasCtx.globalCompositeOperation = "source-over";
+  drawConnectors(canvasCtx, results.poseLandmarks, POSE_CONNECTIONS, { color: "#00FF00", lineWidth: 4 });
+  drawLandmarks(canvasCtx, results.poseLandmarks, { color: "#FF0000", lineWidth: 2 });
+  canvasCtx.restore();
+
+  grid.updateLandmarks(results.poseWorldLandmarks);
+}
+
+const pose = new Pose({
+  locateFile: (file) => {
+    return `https://cdn.jsdelivr.net/npm/@mediapipe/pose/${file}`;
+  },
+});
+pose.setOptions({
+  modelComplexity: 1,
+  smoothLandmarks: true,
+  enableSegmentation: true,
+  smoothSegmentation: true,
+  minDetectionConfidence: 0.5,
+  minTrackingConfidence: 0.5,
+});
+pose.onResults(onResults);
+
+const camera = new Camera(videoElement, {
+  onFrame: async () => {
+    await pose.send({ image: videoElement });
+  },
+  width: 1280,
+  height: 720,
+});
+camera.start();
